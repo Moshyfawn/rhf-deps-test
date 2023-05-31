fc44ebf0e5eff9419c61135aaff0dd1e54b60906
moshyfawn <email@moshyfawn.dev>
2023-05-31 19:03:46 -0400

    repro esm fragment issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm, Controller } from 'react-hook-form'
+import logo from './logo.svg'
+import './App.css'
+
+const onSubmit = (data) => {}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { handleSubmit, control } = useForm()
+	return (
+		<form onSubmit={handleSubmit(onSubmit)} className='App'>
+			<Controller name='file' control={control} render={({ field }) => <input {...field} />} />
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</form>
+	)
 }
 
-export default App;
+export default App
